6a75c33398379145f654bdf71dce6d456a314d91
wonder199999 <hanglv253@yeah.net>
2018-07-23 00:50:14 +0800

    Implemented a preliminary V25/V35 CPU core

--- src/emu/cpu/nec/nec.h
+++ src/emu/cpu/nec/nec.h
@@ -2,22 +2,20 @@
 #ifndef __NEC_H_
 #define __NEC_H_
 
+
 typedef struct _nec_config nec_config;
 struct _nec_config
 {
-	const UINT8*	v25v35_decryptiontable;		// internal decryption table
+	const UINT8*	v25v35_decryptiontable; // internal decryption table
 };
 
-#define NEC_INPUT_LINE_INTP0	(10)
-#define NEC_INPUT_LINE_INTP1	(11)
-#define NEC_INPUT_LINE_INTP2	(12)
-#define NEC_INPUT_LINE_POLL	(20)
+#define NEC_INPUT_LINE_POLL 20
 
 enum
 {
-	NEC_PC = 0,
-	NEC_IP, NEC_AW, NEC_CW, NEC_DW, NEC_BW, NEC_SP, NEC_BP,
-	NEC_IX, NEC_IY, NEC_FLAGS, NEC_ES, NEC_CS, NEC_SS, NEC_DS,
+	NEC_PC=0,
+	NEC_IP, NEC_AW, NEC_CW, NEC_DW, NEC_BW, NEC_SP, NEC_BP, NEC_IX, NEC_IY,
+	NEC_FLAGS, NEC_ES, NEC_CS, NEC_SS, NEC_DS,
 	NEC_VECTOR, NEC_PENDING
 };
 
