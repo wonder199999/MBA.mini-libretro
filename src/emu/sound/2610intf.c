ea8ab63586b22bc1d19f7dfb0b2c07ffb3133525
wonder199999 <hanglv253@yeah.net>
2019-04-06 08:25:19 +0800

    remove YM2610a support

--- src/emu/sound/2610intf.c
+++ src/emu/sound/2610intf.c
@@ -20,19 +20,20 @@
 typedef struct _ym2610_state ym2610_state;
 struct _ym2610_state
 {
-	sound_stream *	stream;
-	emu_timer *		timer[2];
-	void *			chip;
-	void *			psg;
-	const ym2610_interface *intf;
-	running_device *device;
+	const ym2610_interface	*intf;
+	running_device		*device;
+	sound_stream		*stream;
+	emu_timer		*timer[2];
+	void			*chip;
+	void			*psg;
 };
 
 
 INLINE ym2610_state *get_safe_token(running_device *device)
 {
 	assert(device != NULL);
-	assert(device->type() == SOUND_YM2610 || device->type() == SOUND_YM2610B);
+	assert(device->type() == SOUND_YM2610);
+
 	return (ym2610_state *)downcast<legacy_device_base *>(device)->token();
 }
 
@@ -70,11 +71,13 @@ static const ssg_callbacks psgintf =
 };
 
 /*------------------------- TM2610 -------------------------------*/
+
 /* IRQ Handler */
 static void IRQHandler(void *param,int irq)
 {
 	ym2610_state *info = (ym2610_state *)param;
-	if(info->intf->handler) info->intf->handler(info->device, irq);
+	if (info->intf->handler)
+		info->intf->handler(info->device, irq);
 }
 
 /* Timer overflow callback from timer.c */
@@ -93,7 +96,8 @@ static TIMER_CALLBACK( timer_callback_1 )
 static void timer_handler(void *param,int c,int count,int clock)
 {
 	ym2610_state *info = (ym2610_state *)param;
-	if( count == 0 )
+
+	if (count == 0)
 	{	/* Reset FM Timer */
 		timer_enable(info->timer[c], 0);
 	}
@@ -112,7 +116,6 @@ void ym2610_update_request(void *param)
 	ym2610_state *info = (ym2610_state *)param;
 	stream_update(info->stream);
 }
-
 
 static STREAM_UPDATE( ym2610_stream_update )
 {
@@ -137,16 +140,19 @@ static STATE_POSTLOAD( ym2610_intf_postload )
 static DEVICE_START( ym2610 )
 {
 	static const ym2610_interface generic_2610 = { 0 };
-	static const ay8910_interface generic_ay8910 =
-	{
+	static const ay8910_interface generic_ay8910 = {
 		AY8910_LEGACY_OUTPUT | AY8910_SINGLE_OUTPUT,
 		AY8910_DEFAULT_LOADS,
 		DEVCB_NULL, DEVCB_NULL, DEVCB_NULL, DEVCB_NULL
 	};
+
 	const ym2610_interface *intf = device->baseconfig().static_config() ? (const ym2610_interface *)device->baseconfig().static_config() : &generic_2610;
-	int rate = device->clock()/72;
-	void *pcmbufa,*pcmbufb;
-	int  pcmsizea,pcmsizeb;
+	int rate = device->clock() / 72;
+	void *pcmbufa, *pcmbufb;
+
+	int pcmsizea;
+	int pcmsizeb;
+
 	ym2610_state *info = get_safe_token(device);
 	astring name;
 	device_type type = device->type();
@@ -162,12 +168,15 @@ static DEVICE_START( ym2610 )
 
 	/* stream system initialize */
 	info->stream = stream_create(device,0,2,rate,info,(type == SOUND_YM2610) ? ym2610_stream_update : ym2610b_stream_update);
+
 	/* setup adpcm buffers */
 	pcmbufa  = *device->region();
 	pcmsizea = device->region()->bytes();
 	name.printf("%s.deltat", device->tag());
+
 	pcmbufb  = (void *)(memory_region(device->machine, name));
 	pcmsizeb = memory_region_length(device->machine, name);
+
 	if (pcmbufb == NULL || pcmsizeb == 0)
 	{
 		pcmbufb = pcmbufa;
@@ -175,9 +184,9 @@ static DEVICE_START( ym2610 )
 	}
 
 	/**** initialize YM2610 ****/
-	info->chip = ym2610_init(info,device,device->clock(),rate,
-		           pcmbufa,pcmsizea,pcmbufb,pcmsizeb,
-		           timer_handler,IRQHandler,&psgintf);
+	info->chip = ym2610_init(info, device, device->clock(), rate,
+		           pcmbufa, pcmsizea, pcmbufb, pcmsizeb,
+		           timer_handler, IRQHandler, &psgintf);
 	assert_always(info->chip != NULL, "Error creating YM2610 chip");
 
 	state_save_register_postload(device->machine, ym2610_intf_postload, info);
@@ -206,7 +215,7 @@ READ8_DEVICE_HANDLER( ym2610_r )
 WRITE8_DEVICE_HANDLER( ym2610_w )
 {
 	ym2610_state *info = get_safe_token(device);
-	ym2610_write(info->chip, offset & 3, data);
+	ym2610_write(info->chip, offset & 0x03, data);
 }
 
 
@@ -220,7 +229,6 @@ WRITE8_DEVICE_HANDLER( ym2610_data_port_a_w ) { ym2610_w(device, 1, data); }
 WRITE8_DEVICE_HANDLER( ym2610_data_port_b_w ) { ym2610_w(device, 3, data); }
 
 
-
 /**************************************************************************
  * Generic get_info
  **************************************************************************/
@@ -230,33 +238,20 @@ DEVICE_GET_INFO( ym2610 )
 	switch (state)
 	{
 		/* --- the following bits of info are returned as 64-bit signed integers --- */
-		case DEVINFO_INT_TOKEN_BYTES:					info->i = sizeof(ym2610_state);				break;
+		case DEVINFO_INT_TOKEN_BYTES:			info->i = sizeof(ym2610_state); break;
 
 		/* --- the following bits of info are returned as pointers to data or functions --- */
-		case DEVINFO_FCT_START:							info->start = DEVICE_START_NAME( ym2610 );				break;
-		case DEVINFO_FCT_STOP:							info->stop = DEVICE_STOP_NAME( ym2610 );				break;
-		case DEVINFO_FCT_RESET:							info->reset = DEVICE_RESET_NAME( ym2610 );				break;
+		case DEVINFO_FCT_START:				info->start = DEVICE_START_NAME( ym2610 ); break;
+		case DEVINFO_FCT_STOP:				info->stop = DEVICE_STOP_NAME( ym2610 ); break;
+		case DEVINFO_FCT_RESET:				info->reset = DEVICE_RESET_NAME( ym2610 ); break;
 
 		/* --- the following bits of info are returned as NULL-terminated strings --- */
-		case DEVINFO_STR_NAME:							strcpy(info->s, "YM2610");							break;
-		case DEVINFO_STR_FAMILY:					strcpy(info->s, "Yamaha FM");						break;
-		case DEVINFO_STR_VERSION:					strcpy(info->s, "1.0");								break;
-		case DEVINFO_STR_SOURCE_FILE:						strcpy(info->s, __FILE__);							break;
-		case DEVINFO_STR_CREDITS:					strcpy(info->s, "Copyright Nicola Salmoria and the MAME Team"); break;
+		case DEVINFO_STR_NAME:				strcpy(info->s, "YM2610"); break;
+		case DEVINFO_STR_FAMILY:			strcpy(info->s, "Yamaha FM"); break;
+		case DEVINFO_STR_VERSION:			strcpy(info->s, "1.0"); break;
+		case DEVINFO_STR_SOURCE_FILE:			strcpy(info->s, __FILE__); break;
+		case DEVINFO_STR_CREDITS:			strcpy(info->s, "Copyright Nicola Salmoria and the MAME Team"); break;
 	}
 }
-
-DEVICE_GET_INFO( ym2610b )
-{
-	switch (state)
-	{
-		/* --- the following bits of info are returned as NULL-terminated strings --- */
-		case DEVINFO_STR_NAME:							strcpy(info->s, "YM2610B");					break;
-
-		default:										DEVICE_GET_INFO_CALL(ym2610);				break;
-	}
-}
-
 
 DEFINE_LEGACY_SOUND_DEVICE(YM2610, ym2610);
-DEFINE_LEGACY_SOUND_DEVICE(YM2610B, ym2610b);
